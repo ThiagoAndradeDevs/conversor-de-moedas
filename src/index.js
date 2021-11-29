6fd024ee1b63b009ee1a7a28c139c990eef7ecf1
Thiago <thiago.andrade.devs@gmail.com>
2021-11-29 16:03:01 -0300

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ConversorMoedas from './conversor-moedas';
 import reportWebVitals from './reportWebVitals'
-import 'bootstrap/dist/css/bootstrap.min';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <ConversorMoedas />,
