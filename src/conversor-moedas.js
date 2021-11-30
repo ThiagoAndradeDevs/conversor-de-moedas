8de6c9ff3e4a67eb12527d2b810b4dbdd22f8c73
Thiago <thiago.andrade.devs@gmail.com>
2021-11-30 14:55:17 -0300

    atualizando

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,9 +1,42 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './conversor-moedas.css';
+import { Button, Form, Col, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function ConversorMoedas() {
   return (
-    <h1>Conversor de Moedas</h1>
+    <div>
+      <h1>Conversor de Moedas</h1>
+      <div className='Jumbo'>
+        <Form>
+          <Form.Row>
+            <Col sm='3'>
+              <Form.Control
+                placeholder='0'
+                value={1}
+                required />
+            </Col>
+            <Col sm='3'>
+              <Form.Control as='select'>
+              </Form.Control>
+            </Col>
+            <Col sm='1' className='text-center' style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+            <Col sm='3'>
+              <Form.Control as='select'>
+              </Form.Control>
+            </Col>
+            <Col sm='2'>
+              <Button variant='success' type='submit' />
+            </Col>
+          </Form.Row>
+        </Form>
+      </div>
+    </div>
   );
 }
 
