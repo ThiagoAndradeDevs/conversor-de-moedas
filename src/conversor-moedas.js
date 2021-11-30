08a7109e28e25570d9c02ae7709afd92e0f17a14
Thiago <thiago.andrade.devs@gmail.com>
2021-11-30 15:55:14 -0300

    implementando

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,43 +1,42 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable react/jsx-pascal-case */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './conversor-moedas.css';
 import { Button, Form, Col, Spinner } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
-
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 
 function ConversorMoedas() {
   return (
     <div>
       <h1>Conversor de Moedas</h1>
-      <div className='Jumbo'>
+      <div className='jumbo'>
         <Form>
-          <Form.Row>
-            <Col sm='3'>
-              <Form.Control
-                placeholder='0'
-                value={1}
-                required />
-            </Col>
-            <Col sm='3'>
-              <Form.Control as='select'>
-              </Form.Control>
-            </Col>
-            <Col sm='1' className='text-center' style={{ paddingTop: '5px' }}>
+          <h2>
+            <h3 sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </h3>
+            <h3 sm="3">
+              <Form.Control as="select" />
+            </h3>
+            <h3 sm="1"
+              className="text-center" style={{ paddingTop: "5px" }}>
               <FontAwesomeIcon icon={faAngleDoubleRight} />
-            </Col>
-            <Col sm='3'>
-              <Form.Control as='select'>
-              </Form.Control>
-            </Col>
-            <Col sm='2'>
-              <Button variant='success' type='submit' />
-            </Col>
-          </Form.Row>
+            </h3>
+            <h3 sm="3">
+              <Form.Control as="select" />
+            </h3>
+            <h3 sm="2">
+              <Button variant="success" type="submit" />
+            </h3>
+          </h2>
         </Form>
-      </div>
-    </div>
-  );
-}
 
+      </div>
+
+
+    </div>
+  )
+}
 export default ConversorMoedas;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ConversorMoedas from './conversor-moedas';
 import reportWebVitals from './reportWebVitals'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
-  <ConversorMoedas />,
-  document.getElementById('root')
-);
+ReactDOM.render(<ConversorMoedas />, document.getElementById('root'));
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
